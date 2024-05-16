aad141d23468d10355707fafb3732f97e6cce261
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-05-16 16:42:30 +0300

    new changes in boarding

--- app/src/main/java/com/solutionplus/altasherat/features/splash/presention/OnBoardingOneFragment.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/splash/presention/OnBoardingOneFragment.kt
@@ -1,6 +1,7 @@
 package com.solutionplus.altasherat.features.splash.presention
 
 import android.os.Bundle
+import com.solutionplus.altasherat.R
 import com.solutionplus.altasherat.android.helpers.logging.getClassLogger
 import com.solutionplus.altasherat.common.presentation.ui.base.fragment.BaseFragment
 import com.solutionplus.altasherat.databinding.FragmentOnBoardingOneBinding
@@ -20,6 +21,9 @@ class OnBoardingOneFragment : BaseFragment<FragmentOnBoardingOneBinding>() {
 
     override fun viewInit() {
         logger.debug("one")
+        binding.cardView.welcomeText1.text=getString(R.string.welcome_onboarding_1)
+        binding.cardView.welcomeText2.text=getString(R.string.welcome_onboarding_2)
+
     }
     companion object {
         private val logger = getClassLogger()
