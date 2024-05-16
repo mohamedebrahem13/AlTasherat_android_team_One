22d165541d5a6ec54c8d0d71b6cc894760e59244
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2024-05-16 15:39:12 +0300

    add personal info ui

---
+++ app/src/main/java/com/solutionplus/altasherat/features/personal_info/presentation/ui/PersonalInfoFragment.kt
@@ -0,0 +1,21 @@
+package com.solutionplus.altasherat.features.personal_info.presentation.ui
+
+import android.os.Bundle
+import com.solutionplus.altasherat.common.presentation.ui.base.fragment.BaseFragment
+import com.solutionplus.altasherat.databinding.FragmentPersonalInfoBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PersonalInfoFragment : BaseFragment<FragmentPersonalInfoBinding>() {
+    override fun onFragmentReady(savedInstanceState: Bundle?) {
+    }
+
+    override fun onLoading(isLoading: Boolean) {
+    }
+
+    override fun subscribeToObservables() {
+    }
+
+    override fun viewInit() {
+    }
+}
