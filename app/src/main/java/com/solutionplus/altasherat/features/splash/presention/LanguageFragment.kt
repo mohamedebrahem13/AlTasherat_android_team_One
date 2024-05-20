67939d2ba5b046bcd1094b65fcce0839ce8fa95e
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-05-20 16:27:24 +0300

    new changes at repo and viewmodel

--- app/src/main/java/com/solutionplus/altasherat/features/splash/presention/LanguageFragment.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/splash/presention/LanguageFragment.kt
@@ -8,11 +8,47 @@ import android.view.ViewGroup
 import com.solutionplus.altasherat.R
 import com.solutionplus.altasherat.common.presentation.ui.base.fragment.BaseFragment
 import com.solutionplus.altasherat.databinding.FragmentLanguageBinding
+import com.solutionplus.altasherat.features.splash.domain.models.Country
 
 class LanguageFragment : BaseFragment<FragmentLanguageBinding>() {
 
 
     override fun onFragmentReady(savedInstanceState: Bundle?) {
+        val countries = listOf(
+            Country(
+                id = 1,
+                name = "Saudi Arabia",
+                nationality = "Saudi",
+                currency = "SAR",
+                code = "SA",
+                phoneCode = "+966",
+                visible = true,
+                flag = "\uD83C\uDDEA\uD83C\uDDEC"
+            ),
+            Country(
+                id = 2,
+                name = "United Arab Emirates",
+                nationality = "Emirati",
+                currency = "AED",
+                code = "AE",
+                phoneCode = "+971",
+                visible = true,
+                flag = "\uD83C\uDDEA\uD83C\uDDEC"
+            ),
+            Country(
+                id = 3,
+                name = "United States",
+                nationality = "American",
+                currency = "USD",
+                code = "US",
+                phoneCode = "+1",
+                visible = true,
+                flag = "\uD83C\uDDEA\uD83C\uDDEC"
+            ),
+            // Add more countries as needed
+        )
+        val spinnerAdapter = CustomSpinnerAdapter(requireContext(), countries)
+        binding.spinner.adapter = spinnerAdapter
     }
 
     override fun onLoading(isLoading: Boolean) {
