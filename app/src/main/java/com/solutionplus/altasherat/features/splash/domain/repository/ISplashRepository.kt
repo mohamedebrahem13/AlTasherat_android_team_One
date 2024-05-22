8085112978cf9466d3287d3e5971596387821f4d
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-05-22 11:29:04 +0300

    fix onboarding

--- app/src/main/java/com/solutionplus/altasherat/features/splash/domain/repository/ISplashRepository.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/splash/domain/repository/ISplashRepository.kt
@@ -4,9 +4,14 @@ import com.solutionplus.altasherat.features.splash.domain.models.CountriesRespon
 import com.solutionplus.altasherat.features.splash.domain.models.Country
 
 interface ISplashRepository {
-        suspend fun getCountriesFromRemote(params: String) : CountriesResponse// Change the return type as needed
+        suspend fun getCountriesFromRemote(params: String): CountriesResponse // Change the return type as needed
         suspend fun saveCountries(countries: List<Country>)
         suspend fun hasCountryStringKey(): Boolean
         suspend fun getCountriesFromLocal(): List<Country>
 
+        // New methods for user preferences
+        suspend fun saveUserPreferredCountry(country: String)
+        suspend fun getUserPreferredCountry(): String
+        suspend fun saveUserPreferredLanguage(language: String)
+        suspend fun getUserPreferredLanguage(): String
 }
