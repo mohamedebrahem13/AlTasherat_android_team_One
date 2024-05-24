fbee4c71ca95a3879aa5b6f516d4ca0651db8ca1
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-05-24 16:34:37 +0300

    fix testing issue and add more test cases fix viewpager issue

--- app/src/main/java/com/solutionplus/altasherat/features/splash/data/repository/local/SplashLocalDS.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/splash/data/repository/local/SplashLocalDS.kt
@@ -26,10 +26,7 @@ internal class SplashLocalDS(private val preferenceStorage: IKeyValueStorageProv
         )
         // Define Type Token for a List of CountryEntity
         val countryListType = object : TypeToken<List<CountryEntity>>() {}.type
-        if (countriesJson != null) {
-            return  countriesJson.getModelFromJSON(countryListType)
-        }
-        return emptyList()
+        return  countriesJson.getModelFromJSON(countryListType)
     }
     override suspend fun saveUserPreferredCountry(country: String) {
         preferenceStorage.save(key = StorageKeyEnum.USER_PREFERRED_COUNTRY, value = country, type = String::class.java)
