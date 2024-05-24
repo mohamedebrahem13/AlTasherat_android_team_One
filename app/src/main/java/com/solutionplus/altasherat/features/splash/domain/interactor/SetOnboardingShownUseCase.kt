ee43dd658c5d2cc93c23cac6dab7ec735ab0d7f7
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-05-24 13:17:18 +0300

    fix onboarding add viewpager fix navigation add more contract

--- app/src/main/java/com/solutionplus/altasherat/features/splash/domain/interactor/SetOnboardingShownUseCase.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/splash/domain/interactor/SetOnboardingShownUseCase.kt
@@ -1,5 +1,6 @@
 package com.solutionplus.altasherat.features.splash.domain.interactor
 
+import com.solutionplus.altasherat.android.helpers.logging.getClassLogger
 import com.solutionplus.altasherat.common.domain.interactor.BaseUseCase
 import com.solutionplus.altasherat.features.splash.domain.repository.ISplashRepository
 
@@ -9,7 +10,11 @@ class SetOnboardingShownUseCase (
 
     override suspend fun execute(params: Boolean?) {
         params?.run {
+            logger.debug("paramsonBoardinbg$params")
             splashRepository.setOnboardingShown(params)
         }
+    }
+    companion object {
+        private val logger = getClassLogger()
     }
 }
