aae82f192b08205145e885e54be76b0e7e4695c4
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-05-26 21:54:09 +0300

    do some changes

--- app/src/main/java/com/solutionplus/altasherat/features/splash/presention/ui/fragments/OnBoardingFragment.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/splash/presention/ui/fragments/OnBoardingFragment.kt
@@ -7,28 +7,29 @@ import androidx.fragment.app.viewModels
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import com.solutionplus.altasherat.R
 import com.solutionplus.altasherat.android.helpers.logging.getClassLogger
 import com.solutionplus.altasherat.common.presentation.ui.base.fragment.BaseFragment
 import com.solutionplus.altasherat.databinding.FragmentOnboardingBinding
 import com.solutionplus.altasherat.features.home.presentation.HomeActivity
-import com.solutionplus.altasherat.features.splash.presention.ui.adapter.ViewPagerAdapter
+import com.solutionplus.altasherat.features.splash.presention.ui.adapter.OnboardingPageAdapter
 import com.solutionplus.altasherat.features.splash.presention.viewmodels.OnBoardingThreeContract
 import com.solutionplus.altasherat.features.splash.presention.viewmodels.OnBoardingThreeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+data class OnboardingPage(val imageResId: Int, val description: Int)
 
 @AndroidEntryPoint
 class OnBoardingFragment : BaseFragment<FragmentOnboardingBinding>() {
 
     private lateinit var viewPager: ViewPager2
     private lateinit var tabLayout: TabLayout
-    private lateinit var adapter: ViewPagerAdapter
+    private lateinit var adapter: OnboardingPageAdapter
     private val viewModel: OnBoardingThreeViewModel by viewModels()
 
     override fun onFragmentReady(savedInstanceState: Bundle?) {
         viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                // Show the button in fragment 2 and 3, hide it in fragment 1
                 if (position == 0) {
                     binding.buttonPrevious.visibility = View.GONE
                     binding.textPrevious.visibility = View.GONE
@@ -39,26 +40,20 @@ class OnBoardingFragment : BaseFragment<FragmentOnboardingBinding>() {
             }
         })
 
-        // Set click listener for the button
         binding.buttonPrevious.setOnClickListener {
-            // Get the current item position
             val currentPosition = viewPager.currentItem
-            // If it's not the first fragment, move to the previous fragment
             if (currentPosition > 0) {
                 viewPager.currentItem = currentPosition - 1
             }
         }
 
-        // Set click listener for the next button
         binding.buttonNext.setOnClickListener {
-            // Get the current item position
             val currentPosition = viewPager.currentItem
-            // If it's not the last fragment
             if (currentPosition < adapter.itemCount - 1) {
                 viewPager.currentItem = currentPosition + 1
             } else if (currentPosition == adapter.itemCount - 1) {
                 logger.debug("onboardingThree action")
-                viewModel.onActionTrigger( OnBoardingThreeContract.OnBoardingThreeAction.SaveOnboardingShown)
+                viewModel.onActionTrigger(OnBoardingThreeContract.OnBoardingThreeAction.SaveOnboardingShown)
             }
         }
     }
@@ -71,10 +66,10 @@ class OnBoardingFragment : BaseFragment<FragmentOnboardingBinding>() {
             handleSingleEvent(event)
         }
     }
+
     private fun handleSingleEvent(event: OnBoardingThreeContract.OnBoardingThreeEvent) {
-        //Last fragment, you can navigate to HomeActivity if needed
-        when(event){
-            is OnBoardingThreeContract.OnBoardingThreeEvent.NavigateToHome->{
+        when (event) {
+            is OnBoardingThreeContract.OnBoardingThreeEvent.NavigateToHome -> {
                 Intent(requireActivity(), HomeActivity::class.java).also { intent ->
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
                     startActivity(intent)
@@ -83,8 +78,7 @@ class OnBoardingFragment : BaseFragment<FragmentOnboardingBinding>() {
         }
     }
 
-
-        override fun viewInit() {
+    override fun viewInit() {
         viewPager = binding.viewpager
         tabLayout = binding.tabLayout
         setupViewPager()
@@ -92,12 +86,14 @@ class OnBoardingFragment : BaseFragment<FragmentOnboardingBinding>() {
     }
 
     private fun setupViewPager() {
-        val fragments = listOf(
-            OnBoardingOneFragment(),
-            OnBoardingTwoFragment(),
-            OnBoardingThreeFragment()
+        val pages = listOf(
+            OnboardingPage(R.drawable.image_bag, R.string.onboarding_1_welcome_2),
+            OnboardingPage(R.drawable.image_stamp, R.string.onboarding_2_welcome_2) ,
+            OnboardingPage(R.drawable.image_passport,R.string.onboarding_1_welcome_2)
+
+        // Add more pages as needed
         )
-        adapter = ViewPagerAdapter(childFragmentManager, lifecycle, fragments)
+        adapter = OnboardingPageAdapter(requireContext(),pages)
         viewPager.adapter = adapter
     }
 
@@ -105,7 +101,6 @@ class OnBoardingFragment : BaseFragment<FragmentOnboardingBinding>() {
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
         }.attach()
     }
-
 
     companion object {
         private val logger = getClassLogger()
