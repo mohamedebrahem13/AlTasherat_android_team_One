575b48edf3631926ba882c232f71ded82383b328
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2024-05-28 22:59:34 +0300

    update un authorized sheet

--- app/src/main/java/com/solutionplus/altasherat/features/home/presentation/UnAuthorizedDialogFragment.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/home/presentation/UnAuthorizedDialogFragment.kt
@@ -1,11 +1,13 @@
 package com.solutionplus.altasherat.features.home.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.solutionplus.altasherat.databinding.FragmentUnAuthorizedDialogBinding
+import com.solutionplus.altasherat.features.auth.ui.AuthActivity
 
 class UnAuthorizedDialogFragment : BottomSheetDialogFragment() {
 
@@ -21,7 +23,12 @@ class UnAuthorizedDialogFragment : BottomSheetDialogFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        binding.buttonNext.setOnClickListener {
+            Intent(requireActivity(), AuthActivity::class.java).also { intent ->
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
     }
 
     override fun onDestroyView() {
