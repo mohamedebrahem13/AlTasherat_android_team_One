8df4de326cd4c7a415605b06ff4fee5e0ec2e3df
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-05-28 19:43:58 +0300

    language screen

---
+++ app/src/main/java/com/solutionplus/altasherat/features/home/language/LanguageFragment.kt
@@ -0,0 +1,25 @@
+package com.solutionplus.altasherat.features.home.language
+
+import android.os.Bundle
+import com.solutionplus.altasherat.common.presentation.ui.base.fragment.BaseFragment
+import com.solutionplus.altasherat.databinding.FragmentLanguage2Binding
+
+
+class LanguageFragment : BaseFragment<FragmentLanguage2Binding>() {
+    override fun onFragmentReady(savedInstanceState: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoading(isLoading: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun subscribeToObservables() {
+        TODO("Not yet implemented")
+    }
+
+    override fun viewInit() {
+        TODO("Not yet implemented")
+    }
+
+}
