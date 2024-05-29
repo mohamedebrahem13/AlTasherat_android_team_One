3b36b7be288369570a720ae49aca8b0d69398d67
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-05-29 10:38:35 +0300

    remove the un used import

--- app/src/main/java/com/solutionplus/altasherat/features/home/profile/data/repository/remote/ProfileRemoteDataSource.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/home/profile/data/repository/remote/ProfileRemoteDataSource.kt
@@ -3,8 +3,6 @@ package com.solutionplus.altasherat.features.home.profile.data.repository.remote
 import com.solutionplus.altasherat.common.domain.repository.remote.INetworkProvider
 import com.solutionplus.altasherat.features.home.profile.data.models.LogoutResponse
 import com.solutionplus.altasherat.features.home.profile.domain.repository.remote.IProfileRemoteDataSource
-import javax.inject.Inject
-
 internal class ProfileRemoteDataSource (private val networkProvider: INetworkProvider) :IProfileRemoteDataSource{
     override suspend fun logout(): LogoutResponse {
         return networkProvider.delete(
