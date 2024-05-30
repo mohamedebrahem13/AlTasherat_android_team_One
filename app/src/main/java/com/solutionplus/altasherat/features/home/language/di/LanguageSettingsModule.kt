5365d2cbf380c7358504bb978529c7bd77673876
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-05-30 23:28:23 +0300

    language is complete now you can merge

--- app/src/main/java/com/solutionplus/altasherat/features/home/language/di/LanguageSettingsModule.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/home/language/di/LanguageSettingsModule.kt
@@ -10,7 +10,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(ViewModelComponent::class)
