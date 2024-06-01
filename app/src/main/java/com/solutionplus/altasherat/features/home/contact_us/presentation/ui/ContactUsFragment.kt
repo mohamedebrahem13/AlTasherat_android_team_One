edadb5c1922b1291f53547142c45dc16cad437cb
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-06-01 14:41:59 +0300

    check for the new changes

--- app/src/main/java/com/solutionplus/altasherat/features/home/contact_us/presentation/ui/ContactUsFragment.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/home/contact_us/presentation/ui/ContactUsFragment.kt
@@ -3,11 +3,24 @@ package com.solutionplus.altasherat.features.home.contact_us.presentation.ui
 import android.os.Bundle
 import com.solutionplus.altasherat.common.presentation.ui.base.fragment.BaseFragment
 import com.solutionplus.altasherat.databinding.FragmentContactUsBinding
+import com.solutionplus.altasherat.features.services.country.domain.models.Country
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ContactUsFragment : BaseFragment<FragmentContactUsBinding>() {
+    private var countryId: Int? = null
+    private var countryCode: String? = null
+    private var countries: List<Country>? = null
+
     override fun onFragmentReady(savedInstanceState: Bundle?) {
+        binding.etCountryCode.setOnItemClickListener { _, _, position, _ ->
+            val selectedCountryItem = countries?.get(position)
+            selectedCountryItem.let { country ->
+                countryId = country?.id
+                countryCode = country?.phoneCode
+            }
+
+        }
     }
 
     override fun onLoading(isLoading: Boolean) {
