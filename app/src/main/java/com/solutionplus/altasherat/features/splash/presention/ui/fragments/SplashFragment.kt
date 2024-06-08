82ca16af077b7719512318a682b2be23b41a6961
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-06-08 20:21:27 +0300

    fix after merge

--- app/src/main/java/com/solutionplus/altasherat/features/splash/presention/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/solutionplus/altasherat/features/splash/presention/ui/fragments/SplashFragment.kt
@@ -2,7 +2,6 @@ package com.solutionplus.altasherat.features.splash.presention.ui.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.os.LocaleListCompat
 import androidx.fragment.app.viewModels
